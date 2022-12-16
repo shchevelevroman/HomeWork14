68495faa3b1cfd6e83e8adade1d7cf0dbc5ae5b3
a123 <shchevelev.roman@yandex.ru>
2022-12-16 19:55:43 +0300

    домашнее задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,17 @@
 public class Main {
 
-    public static String printBook(String getName, String getBookName, int getYearPublication){
-        System.out.println(Book);
-    }
-    public static int printYearPublication (int getYearPublication){
-        System.out.println(printYearPublication(getYearPublication));
+    public static void printBook(String getName, String getBookName, int getYearPublication){
+        System.out.println("Автор - " + getName + " Название книги - " + getBookName + " Дата публикации - " + getYearPublication);
     }
     public static void main(String[] args) {
 
         Book tolstoy = new Book("Lev Tolstoy", "War and Peas", 1865);
-        printBook();
-        System.out.println("Автор = " + tolstoy.getName());
-        System.out.println("Название книги = " + tolstoy.getBookName());
-        System.out.println("Дата публикации = " + tolstoy.getYearPublication());
+        printBook(tolstoy.getName(), tolstoy.getBookName(), tolstoy.getYearPublication());
+        System.out.println("Корректировка даты публикации");
         tolstoy.setYearPublication(1869);
-        System.out.println("Исправленная дата публикации = " + tolstoy.getYearPublication());
+        printBook(tolstoy.getName(), tolstoy.getBookName(), tolstoy.getYearPublication());
         Book pushkin = new Book("Aleksandr Pushkin", "Evgenii Onegin", 1825);
-        System.out.println("Автор = " + pushkin.getName());
-        System.out.println("Название книги = " + pushkin.getBookName());
-        System.out.println("Дата публикации = " + pushkin.getYearPublication());
+        printBook(pushkin.getName(), pushkin.getBookName(), pushkin.getYearPublication());
+
     }
 }
